a942d2c5526db6486491cab5b04e87eab38a531b
Ocapybara <146412585+MayIyask@users.noreply.github.com>
2026-05-21 10:44:19 +0800

    Create enemies.js

---
+++ data/enemies.js
@@ -0,0 +1,13 @@
+/**
+ * 敌人名称池
+ * 全局变量: ENEMY_NAMES
+ */
+(function(window) {
+  'use strict';
+  
+  window.ENEMY_NAMES = [
+    '异星兽','虚数卫兵','裂界造物','星核猎影',
+    '银鬃铁卫','反物质军团','深渊凝视者','记忆域迷因'
+  ];
+  
+})(window);
